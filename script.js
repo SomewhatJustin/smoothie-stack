c3f626de19bdd8aa39385e3800cb5215048d5544
Justin Fowler <justin.fowler@hey.com>
2022-03-03 15:52:55 -0600

    Created ability to add ingredients and amounts

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-let addBtn = document.getElementById("add-btn")
+const addBtn = document.getElementById("add-btn")
 console.log(addBtn)
+let ingredientInput = ""
+let amountInput = ""
+let ingredientItems = document.getElementById("ingredient-items")
+let amountItems = document.getElementById("amount-items")
+
+function getInputs() {
+  ingredientInput = document.getElementById("ingredient-input").value
+  amountInput = document.getElementById("amount-input").value
+  console.log(`${ingredientInput}, ${amountInput}`)
+}
 
 addBtn.addEventListener("click", function () {
-  console.log("I am alive.")
+  getInputs()
+  ingredientItems.innerHTML += `<p>${ingredientInput}<p>`
+  amountItems.innerHTML += `<p>${amountInput}<p>`
 })
