1649d9506a44f3c36d90f9b87e2485cd26791176
Justin Fowler <justin.fowler@hey.com>
2022-05-26 16:57:43 +0000

    Now saving ingredients, amount, and notes to b64

--- src/ItemPair.js
+++ src/ItemPair.js
@@ -1,37 +1,42 @@
 import React from "react"
 
 export default function ItemPair(props) {
-  function generateNewInputs() {
-    let itemsArray = []
-
-    for (let i = 0; i < props.items.length; i++) {
-      itemsArray.push(
-        <div className="item-pair row">
-          <label className="column">
-            Amount
-            <input
-              type="text"
-              name="amount"
-              value={props.items[i].amount}
-              onChange={(event) => props.setItems(event.target.value)}
-            />
-          </label>
-          <label className="column">
-            Ingredient
-            <input
-              type="text"
-              name="ingredients"
-              value={props.items[i].ingredient}
-              onChange={(event) => props.setItems(event.target.value)}
-            />
-          </label>
-        </div>
-      )
-    }
-    return itemsArray
+  function handleChange(event, myIndex) {
+    props.setItems((prev) => [
+      ...prev.slice(0, myIndex),
+      { ...prev[myIndex], [event.target.name]: event.target.value },
+      ...prev.slice(myIndex + 1),
+    ])
   }
 
-  const formItems = generateNewInputs()
+  // Use ID to determine which index we're at in the Items state array
+  function findMyIndex() {
+    return props.items.findIndex((item) => item.id === props.id)
+  }
+  const myIndex = findMyIndex()
 
-  return <div className="item-holder">{formItems}</div>
+  return (
+    <div className="item-holder">
+      <div className="item-pair row">
+        <label className="column">
+          Amount, {props.id}
+          <input
+            type="text"
+            name="amount"
+            value={props.items[myIndex].amount}
+            onChange={(event) => handleChange(event, myIndex)}
+          />
+        </label>
+        <label className="column">
+          Ingredient
+          <input
+            type="text"
+            name="ingredient"
+            value={props.items[myIndex].ingredient}
+            onChange={(event) => handleChange(event, myIndex)}
+          />
+        </label>
+      </div>
+    </div>
+  )
 }
